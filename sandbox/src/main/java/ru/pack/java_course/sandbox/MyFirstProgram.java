92b990004ef3eddc54da7e96fb5515585ca1dc25
DariaBarabenova <50840615+DariaBarabenova@users.noreply.github.com>
2019-05-27 10:21:02 +0300

    Вычисление расстояния между двумя точками

--- sandbox/src/main/java/ru/pack/java_course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/pack/java_course/sandbox/MyFirstProgram.java
@@ -8,24 +8,56 @@ public class MyFirstProgram {
     hello("user");
     hello("Daria");
 
-    double l = 5;
-    System.out.println("Square squar with side " + l + "= " + area(l));
+    Square s = new Square(5);
+    System.out.println("Square of a square with side " + s.l + "= " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Square af a rectangle with sides " +a+ "and" +b+ "= " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Square of a rectangle with sides " +r.a+ " and" +r.b+ " = " + r.area());
+    System.out.println(Math.sqrt(16));
+
+
+    double c = 2;
+    double d = 3;
+    double e = 5;
+    double f = 7;
+    double t = coordinates2(f,d);
+    double v = coordinates1(c,e);
+
+
+
+    System.out.println("Point1 coodinates = " + c +"," + d + " , Point2 coodinates = " + e +"," + f );
+    System.out.println("Differense1 = " + t + "and" + "Differense2 = " + v);
+    System.out.println("Distance = " + distance(t,v));
+
+
+        }
+
+
+
+  public static void hello (String somebody) {
+
+    System.out.println("Hello " + somebody + "!");
 
   }
-   public static void hello (String somebody) {
 
-            System.out.println("Hello " + somebody + "!");
-
-  }
-  public static double area (double len) {
-    return len*len;
-  }
-  public static double area (double a, double b) {
-    return a*b;
+  public static double area (Rectangle r) {
+    return r.a*r.b;
   }
 
-}
+
+  public static double coordinates1 (double c, double e){
+    return(e-c);
+  }
+
+  public static double coordinates2 (double f, double d){
+    return(f-d);
+  }
+
+  public static double distance (double t, double v){
+    return (Math.sqrt(Math.pow(t, 2)+Math.pow(v, 2)));
+  }
+  }
+
+
+
+
---
+++ sandbox/src/main/java/ru/pack/java_course/sandbox/Points.java
@@ -0,0 +1,13 @@
+package ru.pack.java_course.sandbox;
+
+public class Points {
+
+  public double c;
+  public double d;
+  public double e;
+  public double f;
+
+
+
+
+}
