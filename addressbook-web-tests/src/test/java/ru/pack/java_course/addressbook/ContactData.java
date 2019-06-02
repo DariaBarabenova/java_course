9f74e4f13c7685749d155eb661d94f3b030828e0
DariaBarabenova <Barabenova@mail.ru>
2019-06-02 22:03:37 +0300

    тест для контакта финальный

---
+++ addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.pack.java_course.addressbook;
+
+public class ContactData {
+  private final String ncName;
+  private final String ncLastname;
+  private final String ncTitle;
+  private final String ncCompany;
+  private final String ncHomeTelephone;
+  private final String ncEmail;
+
+  public ContactData(String ncName, String ncLastname, String ncTitle, String ncCompany, String ncHomeTelephone, String ncEmail) {
+    this.ncName = ncName;
+    this.ncLastname = ncLastname;
+    this.ncTitle = ncTitle;
+    this.ncCompany = ncCompany;
+    this.ncHomeTelephone = ncHomeTelephone;
+    this.ncEmail = ncEmail;
+  }
+
+  public String getNcName() {
+    return ncName;
+  }
+
+  public String getNcLastname() {
+    return ncLastname;
+  }
+
+  public String getNcTitle() {
+    return ncTitle;
+  }
+
+  public String getNcCompany() {
+    return ncCompany;
+  }
+
+  public String getNcHomeTelephone() {
+    return ncHomeTelephone;
+  }
+
+  public String getNcEmail() {
+    return ncEmail;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/TestContactCreation.java
+++ addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/TestContactCreation.java
@@ -12,51 +12,61 @@ public class TestContactCreation {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    wd.get("http://localhost/addressbook/group.php");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.id("LoginForm")).click();
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testContactCreation() throws Exception {
-    wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.id("LoginForm")).click();
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-    wd.findElement(By.linkText("add new")).click();
+    goToNewContactPage();
+    fillNewContactForm(new ContactData("Name", "Lastname", "Title", "Company", "1111111", "testemail@test.com"));
+    submitNewContactCreation();
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitNewContactCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+
+  private void fillNewContactForm(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Name");
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getNcName());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Lastname");
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getNcLastname());
     wd.findElement(By.name("title")).click();
     wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys("Title");
+    wd.findElement(By.name("title")).sendKeys(contactData.getNcTitle());
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("Company");
+    wd.findElement(By.name("company")).sendKeys(contactData.getNcCompany());
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("1111111");
+    wd.findElement(By.name("home")).sendKeys(contactData.getNcHomeTelephone());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("testemail@test.com");
+    wd.findElement(By.name("email")).sendKeys(contactData.getNcEmail());
+  }
+
+  private void goToNewContactPage() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
   }
 
   private boolean isAlertPresent() {
