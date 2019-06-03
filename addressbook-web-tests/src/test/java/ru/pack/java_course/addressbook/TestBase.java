2a20f66308db76f6cdff9d40780307a7de872675
DariaBarabenova <Barabenova@mail.ru>
2019-06-03 21:06:54 +0300

    тест на создание контактов преобразован

--- addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/TestBase.java
@@ -93,4 +93,41 @@ public class TestBase {
   protected void selectGroup() {
     wd.findElement(By.name("selected[]")).click();
   }
+
+  protected void logOut() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  protected void goToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  protected void submitNewContactCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  protected void fillNewContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getNcName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getNcLastname());
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(contactData.getNcTitle());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactData.getNcCompany());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getNcHomeTelephone());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getNcEmail());
+  }
+
+  protected void goToNewContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
 }
