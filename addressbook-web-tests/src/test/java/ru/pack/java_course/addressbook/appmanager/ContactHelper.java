27eecb2d79bee11744bcdcc7ac0c1f5da255618d
DariaBarabenova <Barabenova@mail.ru>
2019-06-03 22:09:12 +0300

    созданы помощники

---
+++ addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,42 @@
+package ru.pack.java_course.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.pack.java_course.addressbook.model.ContactData;
+
+public class ContactHelper {
+  private WebDriver wd;
+
+  public ContactHelper(WebDriver wd) {
+    this.wd = wd;
+  }
+
+  public void submitNewContactCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  public void fillNewContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getNcName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getNcLastname());
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(contactData.getNcTitle());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactData.getNcCompany());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getNcHomeTelephone());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getNcEmail());
+  }
+
+  public void goToNewContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+}
--- addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/tests/TestContactCreation.java
+++ addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/tests/TestContactCreation.java
@@ -7,9 +7,9 @@ public class TestContactCreation extends TestBase{
 
   @Test
   public void testContactCreation() throws Exception {
-    app.goToNewContactPage();
-    app.fillNewContactForm(new ContactData("Name", "Lastname", "Title", "Company", "1111111", "testemail@test.com"));
-    app.submitNewContactCreation();
+    app.getContactHelper().goToNewContactPage();
+    app.getContactHelper().fillNewContactForm(new ContactData("Name", "Lastname", "Title", "Company", "1111111", "testemail@test.com"));
+    app.getContactHelper().submitNewContactCreation();
     app.goToHomePage();
     app.logOut();
   }
