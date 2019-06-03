eaf3d091f6c83f0738059ec587ace56b2d7c3de9
DariaBarabenova <Barabenova@mail.ru>
2019-06-03 22:41:27 +0300

    создан общий базовый класс д классов-помощников

--- addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/appmanager/ContactHelper.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.pack.java_course.addressbook.model.ContactData;
 
-public class ContactHelper {
-  private WebDriver wd;
+public class ContactHelper extends HelperBase {
+
 
   public ContactHelper(WebDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void submitNewContactCreation() {
@@ -16,24 +16,12 @@ public class ContactHelper {
   }
 
   public void fillNewContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getNcName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getNcLastname());
-    wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(contactData.getNcTitle());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(contactData.getNcCompany());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.getNcHomeTelephone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getNcEmail());
+    type (By.name("firstname"),contactData.getNcName());
+    type (By.name("lastname"),contactData.getNcLastname());
+    type (By.name("title"),contactData.getNcTitle());
+    type (By.name("company"),contactData.getNcCompany());
+    type (By.name("home"),contactData.getNcHomeTelephone());
+    type (By.name("email"), contactData.getNcEmail());
   }
 
   public void goToNewContactPage() {
