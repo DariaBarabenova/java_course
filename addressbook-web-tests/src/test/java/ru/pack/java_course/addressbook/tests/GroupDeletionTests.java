ce4fbad53c96d31f53611ffd64e23a677042cfd0
DariaBarabenova <Barabenova@mail.ru>
2019-06-10 21:53:48 +0300

    проверка и обеспечение предусловия/тест на удаление группы

--- addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,17 @@
 package ru.pack.java_course.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.pack.java_course.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().seleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
