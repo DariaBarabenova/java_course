c51fa4bda24f8897cd9416dde76c7da4ba9b9ca0
DariaBarabenova <Barabenova@mail.ru>
2019-06-17 22:04:04 +0300

    сравнение списков/коллекций (удаление групп и контактов)

--- addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/pack/java_course/addressbook/model/ContactData.java
@@ -1,5 +1,7 @@
 package ru.pack.java_course.addressbook.model;
 
+import java.util.Objects;
+
 public class ContactData {
   private final String ncName;
   private final String ncLastname;
@@ -17,6 +19,36 @@ public class ContactData {
     this.ncHomeTelephone = ncHomeTelephone;
     this.ncEmail = ncEmail;
     this.group = group;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ContactData that = (ContactData) o;
+    return Objects.equals(ncName, that.ncName) &&
+            Objects.equals(ncLastname, that.ncLastname) &&
+            Objects.equals(ncTitle, that.ncTitle) &&
+            Objects.equals(ncCompany, that.ncCompany) &&
+            Objects.equals(ncHomeTelephone, that.ncHomeTelephone) &&
+            Objects.equals(ncEmail, that.ncEmail);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(ncName, ncLastname, ncTitle, ncCompany, ncHomeTelephone, ncEmail);
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "ncName='" + ncName + '\'' +
+            ", ncLastname='" + ncLastname + '\'' +
+            ", ncTitle='" + ncTitle + '\'' +
+            ", ncCompany='" + ncCompany + '\'' +
+            ", ncHomeTelephone='" + ncHomeTelephone + '\'' +
+            ", ncEmail='" + ncEmail + '\'' +
+            '}';
   }
 
   public String getNcName() {
